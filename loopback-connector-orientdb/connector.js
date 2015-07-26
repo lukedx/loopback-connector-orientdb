9c2e04459e87de769fef1733d6d156966fd93376
lukedx <luke.vinton@datalytyx.com>
2015-07-26 21:12:25 +0100

    Update

---
+++ loopback-connector-orientdb/connector.js
@@ -0,0 +1,14 @@
+﻿// Require the DB driver
+var orient = require('node-graph-service');
+// Require the base SqlConnector class
+var SqlConnector = require('loopback-connector').SqlConnector;
+
+/**
+ * Initialize the  connector against the given data source
+ *
+ * @param {DataSource} dataSource The loopback-datasource-juggler dataSource
+ * @param {Function} [callback] The callback function
+ */
+exports.initialize = function initializeDataSource(dataSource, callback) {
+    throw ("Not implemented");
+};
